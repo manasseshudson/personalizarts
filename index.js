983967d9b20745f837f97761814c3074f0042b22
Manasses <manasseshudson@gmail.com>
2025-09-25 10:04:00 -0300

    add o projeto

---
+++ index.js
@@ -0,0 +1,209 @@
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const { Knex } = require('knex');
+const knex = require('./database/database');
+const base64 = require('base-64');
+const uniqid = require('uniqid'); 
+const uid2 = require('uid2');
+const path = require('path');
+const cors = require('cors');
+
+const { Client } = require('@notionhq/client');
+
+
+require('dotenv').config();
+app.use(cors());
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+const title = 'IBADEJUF EAD'
+const title_adm = ""
+
+app.get('/admin', (req,res)=>{ 
+
+        res.render('admin')    
+    
+    
+})
+
+app.get('/', (req,res)=>{ 
+
+    knex('tb_produto')
+	//.leftJoin('tb_imagens','tb_imagens.id_produto','tb_produto.id')
+	.select()
+    //.where({pagina_principal: 1})
+    //.limit(4)
+	.then(produtos=>{
+        //console.log(produtos);
+		
+		produtos.forEach(prods=> {
+			console.log(prods.imagem_principal)
+		})
+		
+        res.render('index',{
+            produtos: produtos
+        })    
+    })
+    
+	//res.render('index')
+})
+
+app.get('/admin', (req,res)=>{ 
+
+        res.render('/admin')
+    
+	//res.render('index')
+})
+
+app.get('/grafica', (req,res)=>{ 
+
+    knex('tb_produto')
+	.leftJoin('tb_imagens','tb_imagens.id_produto','tb_produto.id')
+	.where({tipo: 'Grafica'})
+    .select('tb_produto.id','tb_imagens.imagem','tb_produto.valor','tb_produto.titulo')
+    .limit(4).then(produtos=>{
+        console.log(produtos);
+        res.render('grafica',{
+			produtos: produtos
+        })    
+    })
+    
+	//res.render('index')
+})
+
+
+app.get('/produto/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        // Busca o produto pelo ID
+        const produto = await knex('tb_produto').where({ id }).first();
+
+        if (!produto) {
+            return res.status(404).send('Produto não encontrado');
+        }
+
+        // Busca as imagens relacionadas ao produto
+        const imagens = await knex('tb_imagens').where({ id_produto: id });
+        const imagem_principal = await knex('tb_imagens').where({ id_produto: id }).andWhere({pagina_principal: 1});
+
+        console.log(imagem_principal)
+		let imagem_principal_= "";
+		
+		if(imagem_principal.length==0){
+			imagem_principal_= ""
+		}else{
+			imagem_principal_ = imagem_principal[0].imagem ;
+		}
+
+        // Renderiza a página com os dados
+        res.render('produtos', {
+            id_produto: id,
+            titulo: produto.titulo,
+            valor: produto.valor,
+            descricao: produto.descricao,
+            imagens,
+            imagem_principal : imagem_principal_
+        });
+
+    } catch (error) {
+        console.error('Erro ao buscar produto:', error);
+        res.status(500).send('Erro interno no servidor');
+    }
+});
+
+
+app.get('/checkout/:id_produto/:qtde', async (req, res) => {
+    const { id_produto, qtde } = req.params;
+
+    try {
+        const produto = await knex('tb_produto').where('id', id_produto).first();
+		
+		const imagem_principal = await knex('tb_imagens').where({ id_produto: produto.id }).andWhere({pagina_principal: 1});
+		console.log(imagem_principal)
+        if (!produto) {
+            return res.status(404).send('Produto não encontrado');
+        }
+
+        const subtotal = qtde * produto.valor;
+
+        const subtotalFormatado = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(subtotal);
+
+        res.render('checkout', {
+            id_produto,
+            qtde,
+            produtos: [produto],
+			imagem: imagem_principal[0].imagem,
+            subtotal: subtotalFormatado
+        });
+
+    } catch (error) {
+        console.error('Erro no checkout:', error);
+        res.status(500).send('Erro interno do servidor');
+    }
+});
+
+
+// Rota para cadastrar produto
+app.post("/produto", async (req, res) => {
+  try {
+    const { titulo, descricao, valor, observacao, lancamentos } = req.body;
+
+    if (!titulo || !valor) {
+      return res.status(400).json({ erro: "Título e valor são obrigatórios!" });
+    }
+
+    const [id] = await knex("tb_produto").insert({
+      titulo,
+      descricao,
+      valor,
+      observacao,
+	  lancamentos: 1
+    });
+
+    res.json({ sucesso: true, id });
+  } catch (err) {
+    console.error("Erro ao inserir produto:", err);
+    res.status(500).json({ erro: "Erro no servidor" });
+  }
+});
+
+
+app.listen( process.env.PORT,()=>{
+	console.log('Api Rodando porta  '+process.env.PORT)
+})
+
+app.get('/integration',(req,res)=>{
+	
+	const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+	(async () => {
+		const blockId = '2741c8683648804d8d9fd412aa418f6a';
+		const response = await notion.blocks.retrieve({
+			block_id: blockId,
+		});
+		console.log(response);
+	})();
+	
+})
+
